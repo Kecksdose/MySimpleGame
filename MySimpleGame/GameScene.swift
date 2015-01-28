a8a1cdd152e42f11a510332709ed47b49dfaa271
Timon Schmelzer <timon.schmelzer@tu-dortmund.de>
2015-01-28 18:01:14 +0100

    added moving monster

--- MySimpleGame/GameScene.swift
+++ MySimpleGame/GameScene.swift
@@ -10,7 +10,34 @@ import SpriteKit
 
 class GameScene: SKScene {
   
+  // some constants which will change difficulty of the game
+  let minSpeedMonster = CGFloat(2.0)
+  let maxSpeedMonster = CGFloat(4.0)
+  
   let player = SKSpriteNode(imageNamed: "player")
+  
+  func random() -> CGFloat{
+    return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+  }
+  
+  func random(#min: CGFloat, max: CGFloat) -> CGFloat{
+    return random() * (max - min) + min
+  }
+  
+  func addMonster(){
+    let monster = SKSpriteNode(imageNamed: "monster")
+  
+    let actualY = random(min: monster.size.height/2, max: size.height - monster.size.height/2)
+    monster.position = CGPoint(x: size.width + monster.size.width/2, y: actualY)
+    
+    addChild(monster)
+    
+    let actualDuration = random(min: minSpeedMonster, max: maxSpeedMonster)
+    
+    let actionMove = SKAction.moveTo(CGPoint(x: -monster.size.width/2, y: actualY), duration: NSTimeInterval(actualDuration))
+    let actionMoveDone = SKAction.removeFromParent()
+    monster.runAction(SKAction.sequence([actionMove, actionMoveDone]))
+  }
   
   override func didMoveToView(view: SKView) {
     backgroundColor = SKColor.whiteColor()
@@ -18,8 +45,9 @@ class GameScene: SKScene {
     
     addChild(player)
     }
-    
+  
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
+  
 }
